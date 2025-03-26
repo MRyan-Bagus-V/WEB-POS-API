e5d93a6e79fa4f4ab7e536ba8caafb188fc56988
M Ryan Bagus Valentin <75926612+MRyan-Bagus-V@users.noreply.github.com>
2025-03-27 01:46:40 +0700

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,9 +7,9 @@
       </div>
 
       <!-- Slider Section -->
-      <section class="slider_section" style="margin-top=20px;">
+      <section class="slider_section">
         <div id="customCarousel1" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
+          <div class="carousel-inner" style="margin-top=100px;>
             <!-- Slide 1 -->
             <div class="carousel-item active">
               <div class="container">
