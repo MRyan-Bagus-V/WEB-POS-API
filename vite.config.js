8eb535cd1e8eca669f9b75c0503b3d3f22a41050
M Ryan Bagus Valentine <75926612+MRyanBagusV@users.noreply.github.com>
2025-03-27 00:18:22 +0700

    Menyiapkan deploy GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/MRyan-Bagus-V/WEB-POS-API.git", // Ganti dengan nama repositorimu
   plugins: [
     vue(),
     vueDevTools(),
