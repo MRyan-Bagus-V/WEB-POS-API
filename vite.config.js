11eeec404bfed0c5f3d349f7b678d962319107a9
M Ryan Bagus Valentin <75926612+MRyan-Bagus-V@users.noreply.github.com>
2025-03-27 01:30:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/WEB-POS-API/", // Ganti dengan nama repositorimu
+  base: '/', // Ganti dengan nama repositorimu
   plugins: [
     vue(),
     vueDevTools(),
